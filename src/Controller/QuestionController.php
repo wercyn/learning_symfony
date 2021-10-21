84e59820d75e81b0ea65396dec82508ca18ffa07
wercyn <wercyngatorykss@gmail.com>
2021-10-21 22:35:25 +0200

    simple testig twig template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,6 +21,17 @@ class QuestionController
      */
     public function show($question)
     {
-        return new Response('Future page to show: '.$question);
+
+        $answers = [
+            'testowa odpwoeidz na zadane pytanie',
+            'głupia odpowiedz człowiekasfrustrowanego życiem',
+            'najgorsza firma to trol intermedia'
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $question)),
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'answers' => $answers
+        ]);
     }
 }
